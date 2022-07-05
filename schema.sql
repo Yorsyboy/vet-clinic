d53ea9b63dec090dbd8a85e915733d7b36490e64
yorsyboy <adekunletaiwo43@yahoo.com>
2022-07-05 09:25:37 +0100

    add schema.sql file

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id integer NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg REAL NOT NULL
+);
