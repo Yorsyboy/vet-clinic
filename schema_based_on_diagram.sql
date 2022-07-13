44fd71394ebe619cfdb5eb151bb948675dff9a80
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-07-13 11:58:11 +0100

    create mediacl_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ CREATE TABLE patients(
     name VARCHAR(50),
     date_of_birth DATE NOT NULL
 )
+
+CREATE TABLE medical_histories(
+    id SERIAL PRIMARY KEY,
+     admitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL,
+    status VARCHAR(50),
+    CONSTRAINT patients_fk FOREIGN KEY (patient_id) REFERENCES patients(id)
+)
