f5d9bbb2023697c28a1a5842796d77e422a2c1f7
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-07-13 11:58:36 +0100

    create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,12 @@ CREATE TABLE medical_histories(
     status VARCHAR(50),
     CONSTRAINT patients_fk FOREIGN KEY (patient_id) REFERENCES patients(id)
 )
+
+CREATE TABLE invoices(
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id INT NOT NULL,
+    CONSTRAINT history_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+)
